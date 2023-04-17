2f90801825ce715821370a2de05b4ab2fffd9c00
Sanskar Chaurey <84661556+Sanskar-Chaurey@users.noreply.github.com>
2023-04-18 01:30:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// Objects :- 
+
+let animals = {
+    name: 'cow',
+    legs: 4
+}
+
+// console.log(animals);
+
+// dot notation
+console.log(animals.name);
+// bracket notation
+console.log(animals["legs"])
+
+
+// ------------------------------------------------------------------------------
+// Arrays:-
+
+let selectBooks = ["rich dad poor dad", "think And grow rich", "how to improve your self"];
+console.log(selectBooks.length);
+
+
+// ------------------------------------------------------------------------------
+// Function:-
+
+function namasteWorld(name, lastName){
+    console.log("Namaste " + name + " " + lastName);
+}
+
+namasteWorld("sanskar", "chaurey")
+namasteWorld("himanshu", "mishra")
+
+// -----------------------------------------------------------------------
